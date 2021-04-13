6d246e9a890dc46d60e2b3f744774ddc82407c13
micgogi <rahul.gogyani@gmail.com>
2021-04-13 16:42:18 +0530

    😑😑

---
+++ src/test/java/com/micgogi/azureaddemo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.micgogi.azureaddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
